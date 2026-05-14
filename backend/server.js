1ef34880befc835382950f0c37a6de27beaf5217
Hawaz <hawaz4321@gmail.com>
2026-05-14 17:55:26 +0300

    full stack task manager

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors({
+  origin: "*"
+}));
+app.use(express.json());
+
+app.put("/tasks/:id", async (req, res) => {
+  const { task } = req.body;
+
+  await Task.findByIdAndUpdate(req.params.id, { task });
+
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+
+  task.completed = !task.completed;
+  await task.save();
+
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// MongoDB connection
+mongoose
+  .connect(
+    "mongodb+srv://Hawaz:Pawli4321@cluster0.4dt104y.mongodb.net/taskmanager?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Schema
+const taskSchema = new mongoose.Schema({
+  task: String,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// Model
+const Task = mongoose.model("Task", taskSchema);
+
+// Get tasks
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// Add task
+app.post("/tasks", async (req, res) => {
+  const newTask = new Task({
+    task: req.body.task,
+  });
+
+  await newTask.save();
+
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// Delete task
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:5000/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  const addTask = async () => {
+    if (!input.trim()) return;
+
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ task: input }),
+  });
+
+  const data = await res.json();
+  setTasks(data);
+  setInput("");
+  };
+
+  const editTask = async (id, currentTask) => {
+    const newTask = prompt("Edit task:", currentTask);
+
+    if (!newTask || newTask.trim() === "") return;
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ task: newTask }),
+    });
+
+  const data = await res.json();
+  setTasks(data);
+};
+  
+  const toggleComplete = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PATCH",
+    });
+
+    const data = await res.json();
+    setTasks(data);
+  };
+  const deleteTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
+
+    const data = await res.json();
+    setTasks(data);
+  };
+
+return (
+  <div className="app">
+    <div className="container">
+      <h1>Task Manager</h1>
+
+      <div className="input-area">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter a task..."
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li key={task._id} className="task-item">
+            <span
+              className={task.completed ? "completed" : ""}
+            >
+              {task.task}
+            </span>
+
+            <div className="actions">
+              <button onClick={() => toggleComplete(task._id)}>
+                {task.completed ? "Undo" : "Done"}
+              </button>
+
+              <button onClick={() => editTask(task._id, task.task)}>
+                Edit
+              </button>
+
+              <button onClick={() => deleteTask(task._id)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);}
